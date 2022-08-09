45a83086eda92e8993d804c56fcbc5e1d4357cb9
Pogosov <serj@mobalyticshq.com>
2022-08-09 17:35:53 +0200

    updated API headers

--- src/lol/api/api.ts
+++ src/lol/api/api.ts
@@ -5,6 +5,7 @@ const staticDataClient = new GraphQLClient({
   url: 'https://extwidget.mobalytics.gg/lol-static-gql',
   headers: {
     'Accept-Language': 'en_us',
+    Authorization: 'Bearer 2bd985cc-dc15-4cad-bea7-0c9669751e08',
   }
 });
 
@@ -13,6 +14,7 @@ const dynamicDataClient = new GraphQLClient({
   url: 'https://extwidget.mobalytics.gg/lol-gql',
   headers: {
     'Accept-Language': 'en_us',
+    Authorization: 'Bearer 2bd985cc-dc15-4cad-bea7-0c9669751e08',
   }
 });
 
