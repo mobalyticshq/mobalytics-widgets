004b1e69f71009fc77088271ddc1ad79db48f3da
serj <serj@mobalyticshq.com>
2023-05-02 15:41:07 +0200

    updated CDN url

--- src/lol/config.ts
+++ src/lol/config.ts
@@ -1,8 +1,9 @@
-export const CDN_LOL_IMG_PATH = 'https://fastcdn.mobalytics.gg/assets/lol/images';
+export const CDN_LOL_IMG_PATH = 'https://cdn.mobalytics.gg/assets/lol/images';
 export const CDN_CGI_LOL_IMG_PATH = 'https://cdn.mobalytics.gg/cdn-cgi/image';
-export const CDN_COMMON_IMG_PATH = 'https://fastcdn.mobalytics.gg/assets/common/images';
-export const CDN_COMMON_ICON_PATH = 'https://fastcdn.mobalytics.gg/assets/common/icons';
-export const CDN_LOL_VIDEO_PATH = 'https://fastcdn.mobalytics.gg/assets/lol/video';
+export const CDN_COMMON_IMG_PATH = 'https://cdn.mobalytics.gg/assets/common/images';
+export const CDN_COMMON_ICON_PATH = 'https://cdn.mobalytics.gg/assets/common/icons';
+export const CDN_LOL_VIDEO_PATH = 'https://cdn.mobalytics.gg/assets/lol/video';
+export const CDN_VERSION = 'v0.0.1';
 
 export const MOBA_APP_LINK = 'https://app.mobalytics.gg';
 export const MOBA_HOME_URL = 'https://app.mobalytics.gg/lol';
--- src/lol/utils/images.ts
+++ src/lol/utils/images.ts
@@ -1,57 +1,63 @@
-import { CDN_CGI_LOL_IMG_PATH, CDN_COMMON_ICON_PATH, CDN_COMMON_IMG_PATH, CDN_LOL_IMG_PATH } from '../config';
+import {
+  CDN_CGI_LOL_IMG_PATH,
+  CDN_COMMON_ICON_PATH,
+  CDN_COMMON_IMG_PATH,
+  CDN_LOL_IMG_PATH,
+  CDN_VERSION,
+} from '../config';
 import { Rolename } from '../types/gql-dynamic/globalTypes';
 
 export function championImage(slug: string): string {
-  return `${CDN_LOL_IMG_PATH}/dd/champions/icons/${slug.toLowerCase()}.png`;
+  return `${CDN_LOL_IMG_PATH}/dd/champions/icons/${slug.toLowerCase()}.png?${CDN_VERSION}`;
 }
 
 export function summonerSpellIcon(slug: number | string): string {
-  return `${CDN_LOL_IMG_PATH}/dd/summoner-spells/${slug}.png`;
+  return `${CDN_LOL_IMG_PATH}/dd/summoner-spells/${slug}.png?${CDN_VERSION}`;
 }
 
 export function championAbilityImage(slug: string): string {
-  return `${CDN_LOL_IMG_PATH}/dd/champions/abilities/${slug}.png`;
+  return `${CDN_LOL_IMG_PATH}/dd/champions/abilities/${slug}.png?${CDN_VERSION}`;
 }
 
 export function gameItemIcon(slug: number | string): string {
-  return `${CDN_LOL_IMG_PATH}/dd/game-items/${slug}.png`;
+  return `${CDN_LOL_IMG_PATH}/dd/game-items/${slug}.png?${CDN_VERSION}`;
 }
 
 export function perkIcon(slug: number | string): string {
   // we can't use ddragon image here because they have lower quality then ours
-  return `${CDN_LOL_IMG_PATH}/perks/${slug}.png`;
+  return `${CDN_LOL_IMG_PATH}/perks/${slug}.png?${CDN_VERSION}`;
 }
 
 export function roleNameIcon(roleName: Rolename): string {
-  return `${CDN_LOL_IMG_PATH}/rolename-icon/${roleName}.svg`;
+  return `${CDN_LOL_IMG_PATH}/rolename-icon/${roleName}.svg?${CDN_VERSION}`;
 }
 
 export function mobalyticsLogoSymbol(): string {
-  return `${CDN_COMMON_IMG_PATH}/logo/moba-logo-symbol-beta.svg`;
+  return `${CDN_COMMON_IMG_PATH}/logo/moba-logo-symbol-beta.svg?${CDN_VERSION}`;
 }
 
 export function championPosterImage(slug: string): string {
-  return `${CDN_LOL_IMG_PATH}/dd/champions/backgrounds/${slug.toLowerCase()}.jpg`;
+  return `${CDN_LOL_IMG_PATH}/dd/champions/backgrounds/${slug.toLowerCase()}.jpg?${CDN_VERSION}`;
 }
 
 export function championSmallPosterImage(slug: string): string {
-  return `${CDN_LOL_IMG_PATH}/champions/bg-sm/${slug.toLowerCase()}.jpg`;
+  return `${CDN_LOL_IMG_PATH}/champions/bg-sm/${slug.toLowerCase()}.jpg?${CDN_VERSION}`;
 }
 
 export function amumuCrying(): string {
-  return `${CDN_LOL_IMG_PATH}/ui/game-items/items-list-empty.png`;
+  return `${CDN_LOL_IMG_PATH}/ui/game-items/items-list-empty.png?${CDN_VERSION}`;
 }
 
 export function emoteImage(name: string): string {
-  return `${CDN_LOL_IMG_PATH}/ui/emotes/${name}`;
+  return `${CDN_LOL_IMG_PATH}/ui/emotes/${name}?${CDN_VERSION}`;
 }
 
 export function championPosterW400Image(slug: string): string {
-  return `${CDN_CGI_LOL_IMG_PATH}/fit=cover,width=400/assets/lol/images/dd/champions/backgrounds/${slug.toLowerCase()}.jpg`;
+  return `${CDN_CGI_LOL_IMG_PATH}/fit=cover,width=400/assets/lol/images/dd/champions/backgrounds/${slug.toLowerCase()}.jpg?${CDN_VERSION}`;
 }
 
 export function championClassIcon(slug: string) {
-  return `${CDN_LOL_IMG_PATH}/champions-classes/${slug}.svg`;
+  return `${CDN_LOL_IMG_PATH}/champions-classes/${slug}.svg?${CDN_VERSION}`;
 }
 
 /**
@@ -63,5 +69,5 @@ export function championClassIcon(slug: string) {
  *    ?1 in the end of URL need for reset cache after gold color was updated
  */
 export function hexTierIcon(tier: string): string {
-  return `${CDN_COMMON_ICON_PATH}/hex-tiers/${tier.toUpperCase()}.svg`;
+  return `${CDN_COMMON_ICON_PATH}/hex-tiers/${tier.toUpperCase()}.svg?${CDN_VERSION}`;
 }
